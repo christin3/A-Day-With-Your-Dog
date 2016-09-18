9b83f4be34f9cb5b6f0d855ca9e0a9d92413fd1b
Nigel Finley <nfinleymusic@gmail.com>
2016-09-18 17:35:20 -0500

    adding basic marker info, still working on making it work

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,14 +1,14 @@
 // ====== Questions =========
 
 
- // ========Initialize Firebase=========
+// ========Initialize Firebase=========
 
 var config = {
-  apiKey: "AIzaSyAv-6bOhqHPkkst-Aw7ULPb-xvTqHqTpeo",
-  authDomain: "bars-backup.firebaseapp.com",
-  databaseURL: "https://bars-backup.firebaseio.com",
-  storageBucket: "bars-backup.appspot.com",
-  messagingSenderId: "149078553518"
+    apiKey: "AIzaSyAv-6bOhqHPkkst-Aw7ULPb-xvTqHqTpeo",
+    authDomain: "bars-backup.firebaseapp.com",
+    databaseURL: "https://bars-backup.firebaseio.com",
+    storageBucket: "bars-backup.appspot.com",
+    messagingSenderId: "149078553518"
 };
 firebase.initializeApp(config);
 
@@ -18,188 +18,262 @@ var databasePush = firebase.database().ref().push();
 // Initialize variable for any database querying
 var dbQuery = firebase.database();
 
-// Create a new GeoFire instance at the random Firebase location
-var geoFire = new GeoFire(databasePush);
-
-// Initialize the variable for the AUSTIN PARK API push
-
 // Global variables of pos (which is the user location and username in order to retrieve later)
 var pos;
 // Because our map is at a zoom of 13 which is 2 miles, 2 miles in km is 3.21869 Km / 2 = 1.609345
 // Will use this radius to determine what results are populated into the map and cards (only this <= this radiu)
-var resultRadius = 1; // distance in miles or our user location
+var resultRadius = 5; // distance in miles or our user location
 var resultsArray = [];
 
-// Array for the park info
-var parkInfo = [];
-
+var locationMarkers = [];
+// var map; 
 
 // ================ FUNCTIONS =================
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 13
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      console.log("Retrieved user's location: [" + pos.lat + ", " + pos.lng + "]");
-      // Setting the coordinates of user in local storage
-      localStorage.setItem("userLocation", JSON.stringify(pos));
-      
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+    var styles = [{
+        "featureType": "all",
+        "elementType": "labels",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#8ac440"
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [{
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "road.arterial",
+        "elementType": "labels.text",
+        "stylers": [{
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "road.local",
+        "elementType": "labels.text",
+        "stylers": [{
+            "visibility": "on"
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#2196F3"
+        }]
+    }];
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 13,
+        styles: styles,
+        mapTypeControl: false
     });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+    var infoWindow = new google.maps.InfoWindow();
+
+    // The default marker color
+    var defaultIcon = makeMarkerIcon('0091ff');
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log("This is the user's location: [" + pos.lat + ", " + pos.lng + "]");
+            // Setting the coordinates of user in local storage
+            localStorage.setItem("userLocation", JSON.stringify(pos));
+            // creates the user location marker
+            var myMarker = new google.maps.Marker({
+                map: map,
+                position: pos,
+                title: 'You and your Dog are Here!',
+                animation: google.maps.Animation.DROP,
+                icon: defaultIcon,
+
+            });
+            // Create an onclick event to open the large infowindow at each marker.
+            myMarker.addListener('click', function() {
+                populateInfoWindow(this, infoWindow);
+            });
+            // Two event listeners - one for mouseover, one for mouseout,
+            // to change the colors back and forth.
+            myMarker.addListener('mouseover', function() {
+                this.setIcon(highlightedIcon);
+            });
+            myMarker.addListener('mouseout', function() {
+                this.setIcon(defaultIcon);
+            });
+
+            infoWindow.setPosition(pos);
+            
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 };
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 };
 
-function getDistanceInKm(lat2, lng2){
-  var  userLocation = JSON.parse(localStorage.getItem("userLocation", pos));
-  console.log("This is the ARRAY user location: " +  userLocation);
-  var lat1= userLocation.lat;
-  var lng1 = userLocation.lng;
-  var R = 3959; // Radius of the earth in miles
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLng = deg2rad(lng2-lng1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLng/2) * Math.sin(dLng/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var distance = R * c; // Distance in km
-  return distance;
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+        });
+    }
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
+    return markerImage;
+}
+
+
+
+// Haversine function that calculates the distance between to coordinates with the result in miles
+function getDistanceInKm(lat2, lng2) {
+    var userLocation = JSON.parse(localStorage.getItem("userLocation", pos));
+    console.log("This is the ARRAY user location: " + userLocation);
+    var lat1 = userLocation.lat;
+    var lng1 = userLocation.lng;
+    var R = 3959; // Radius of the earth in miles
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLng = deg2rad(lng2 - lng1);
+    var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var distance = R * c; // Distance in km
+    return distance;
 };
 
 
 
 function deg2rad(degrees) {
-  return degrees * (Math.PI /180); 
-  
+    return degrees * (Math.PI / 180);
+
 };
+
+function showMarkers() {
+        // var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < locationMarkers.length; i++) {
+          locationMarkers[i].setMap(map);
+          // bounds.extend(markers[i].position);
+        }
+        // map.fitBounds(bounds);
+      }
+
+// Maybe put all of this in an document on ready function
 // This will handle the user click in the dropdown
 $('.categories a').on('click', getData);
 
 // function to get the user click
-function getData (){
-  var category = $(this).data('category');
-  console.log("User Click: " + category);
+function getData() {
+    var category = $(this).data('category');
+    console.log("User Click: " + category);
 
 
 
 
-// Loop through users in order with the forEach() method. The callback provided
-// to will be called synchronously with a DataSnapshot for each child:
-// Pull out the information of the bars = then use the haversine forlumla to compare user Location and bar location. 
-// Also need to set a radius to calculate distance (look at Geofire to see if call calculate it)
-var query = dbQuery.ref($(this).data('category'));
-// Tie this query to the user click data-category "bars" for example
-query.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      // key will be the item the first time and the second item the second time
-      var key = childSnapshot.key;
-      // child lat and lng will be the actual contents of the child loc
-      var childData = childSnapshot.val();
-    
-      var lat2= childData.loc.lat;
-      var lng2  = childData.loc.lng;
-      // Call the haversine formula that is defined above
-      
-      
-      var distance =  getDistanceInKm(lat2, lng2);         
-      console.log("This is the distance from user location to bar: " + distance);
-      // Add the if logic to take the bar child if the radius is <= to result Radius
-      if(distance <= resultRadius) {
-        console.log("This is the bar closest to you: " + childSnapshot.val().id);
-        console.log("This is the bar closest to you: " + JSON.stringify(childSnapshot.val()));
+    // Loop through users in order with the forEach() method. The callback provided
+    // to will be called synchronously with a DataSnapshot for each child:
+    // Pull out the information of the bars = then use the haversine forlumla to compare user Location and bar location. 
+    // Also need to set a radius to calculate distance (look at Geofire to see if call calculate it)
+    var query = dbQuery.ref($(this).data('category'));
+    // Tie this query to the user click data-category "bars" for example
+    query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                // key will be the item the first time and the second item the second time
+                var key = childSnapshot.key;
+                // child lat and lng will be the actual contents of the child loc
+                var childData = childSnapshot.val();
 
-        // Push all of the relevant data to the page
-      //   resultsArray.push(resultsData);
-      } else {
-        console.log ("This bar is not close to you");
-      }
+                var lat2 = childData.loc.lat;
+                var lng2 = childData.loc.lng;
+                // Call the haversine formula that is defined above
 
-  });
-});
+
+                var distance = getDistanceInKm(lat2, lng2);
+                console.log("This is the distance from user location to bar: " + distance);
+                // Add the if logic to take the bar child if the radius is <= to result Radius
+                if (distance <= resultRadius) {
+                    console.log("This is ID of the bar/park closest to you: " + JSON.stringify(childSnapshot.val().id));
+                    console.log("This is the bar closest to you: " + JSON.stringify(childSnapshot.val()));
+                    console.log("------------------------------");
+
+                    // Push all of the relevant data to the page
+                    //   resultsArray.push(resultsData);
+                    // ======== MAP MARKER CREATION AND PUSH ========
+                    var markerPosition = childSnapshot.val().loc;
+                    var markerTitle = childSnapshot.val().name;
+                    var markerId = childSnapshot.val().id;
+                    var marker = new google.maps.Marker({
+                      // setMap: map,
+                      position: markerPosition,
+                      title: markerTitle,
+                      animation: google.maps.Animation.DROP,
+                      // icon: defaultIcon,
+                      id: markerId
+                    });
+                    // Push the marker to our array of markers.
+                    locationMarkers.push(marker);
+                    console.log("Marker: " + JSON.stringify(locationMarkers));
+                    // marker.setMap(map);
+                    showMarkers();
+
+                    // ======= END MARKER CREATION ===========
+
+                    // ======= TEMPLATE CREATION ==============
+
+
+                } else {
+                    console.log("This bar is not close to you");
+                }
+
+            });
+        });
 };
 
 
-// ======== AUSTIN PARKS API===============
-
-
-// Austin Parks API
-// $.ajax({
-//     url: "https://data.austintexas.gov/resource/up6y-6ww4.json",
-//     type: "GET",
-//     data: {
-//         "$limit": 5000,
-//         "$$app_token": "BpQTL15Fi3PePMZUluMK7cKFy"
-//     }
-// }).done(function (response) {
-
-
-//     var info = response;
-
-
-//     console.log(response);
-
-//     // Cleaning up data from the API
-//     for (i = 0; i < response.length; i++) {
-
-//         response[i].descriptio = response[i].descriptio.replace(
-//                 /<DIV>|<BR>|<\/DIV>|<div dir="ltr">|<a href.*|<IMG src.*/ig, '');
-
-//         response[i].descriptio = response[i].descriptio.replace(
-//                 /LnAustin/ig, "Ln Austin");
-
-//         response[i].descriptio = response[i].descriptio.replace(
-//                 /TerraceAustin/ig, "Terrace Austin");
-
-//         response[i].descriptio = response[i].descriptio.replace(
-//                 /Blvd.Austin/ig, "Blvd Austin");
-
-//     }
-//     // ============= Put all of this in a FOR loop =========
-
-//     // For loop to push all of the items from the Austin parks API into our database
-//     for (var i=0; i < response.length; i++){
-//       var address = response[i].descriptio; 
-//       var name = response[i].name;
-//       var loc = {
-//             lat: response[i].the_geom.coordinates[1],
-//             lng: response[i].the_geom.coordinates[0]
-//       };
-//       dbQuery.ref('parks').push({
-//         address: address,
-//         name: name, 
-//         loc: loc,
-//       });
-//     };
-// });
-
-
-// ========= END AUSTIN PARKS API =================
 
 
 
