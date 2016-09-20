0976df6c1e5a55d8a1260de9bd5bd801ed2ac291
Nigel Finley <nfinleymusic@gmail.com>
2016-09-19 23:26:42 -0500

    adding additional fucntionality for markers

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -169,7 +169,7 @@ function makeMarkerIcon(markerColor) {
 // Haversine function that calculates the distance between to coordinates with the result in miles
 function getDistanceInKm(lat2, lng2) {
     var userLocation = JSON.parse(localStorage.getItem("userLocation", pos));
-    console.log("This is the ARRAY user location: " + userLocation);
+    // console.log("This is the ARRAY user location: " + userLocation);
     var lat1 = userLocation.lat;
     var lng1 = userLocation.lng;
     var R = 3959; // Radius of the earth in miles
@@ -192,39 +192,41 @@ function deg2rad(degrees) {
 };
 
 function showMarkers() {
-    var bounds = new google.maps.LatLngBounds();
+    // var bounds = new google.maps.LatLngBounds();
     // Extend the boundaries of the map for each marker and display the marker
     for (var i = 0; i < locationMarkers.length; i++) {
         locationMarkers[i].setMap(map);
-        bounds.extend(locationMarkers[i].position);
+        // bounds.extend(locationMarkers[i].position);
     }
-    map.fitBounds(bounds);
+    // We might have an issue with this because if there is only one closeby it zooms really far in
+    // map.fitBounds(bounds);
 };
 
-// This function will loop through the listings and hide them all.
+// This function will loop through the listings and hide them all and empty the array location markers.
 function hideMarkers() {
     // Put this within an if statement to run only when there is 
     // information within the array
     for (var i = 0; i < locationMarkers.length; i++) {
         locationMarkers[i].setMap(null);
     }
-    locationMarkers.empty();
+    locationMarkers = [];
 };
 
 // Maybe put all of this in an document on ready function
 // This will handle the user click in the dropdown
 $('.categories a').on('click', getData);
 
-// $('.categories a').on('click', function () {
-//     getData();
-// });
 
-hideMarkers();
+
+
 
 // function to get the user click
 function getData() {
+    hideMarkers();
+    // moves the user window to the map section when the user makes a selection
+    window.location.hash = "yourMap";
     var category = $(this).data('category');
-    console.log("User Click: " + category);
+    // console.log("User Click: " + category);
 
     // Loop through users in order with the forEach() method. The callback provided
     // to will be called synchronously with a DataSnapshot for each child:
@@ -246,15 +248,13 @@ function getData() {
 
 
                 var distance = getDistanceInKm(lat2, lng2);
-                console.log("This is the distance from user location to bar: " + distance);
+                // console.log("This is the distance from user location to bar: " + distance);
                 // Add the if logic to take the bar child if the radius is <= to result Radius
                 if (distance <= resultRadius) {
-                    console.log("This is ID of the bar/park closest to you: " + JSON.stringify(childSnapshot.val().id));
-                    console.log("This is the bar closest to you: " + JSON.stringify(childSnapshot.val()));
-                    console.log("------------------------------");
-
-                    // Push all of the relevant data to the page
-                    //   resultsArray.push(resultsData);
+                    // console.log("This is ID of the " + category + " closest to you: " + JSON.stringify(childSnapshot.val().id));
+                    // console.log("This is the " + category + " closest to you: " + JSON.stringify(childSnapshot.val()));
+                    // console.log("------------------------------");
+;
                     // ======== MAP MARKER CREATION AND PUSH ========
                     var markerPosition = childSnapshot.val().loc;
                     var markerTitle = childSnapshot.val().name;
@@ -269,22 +269,28 @@ function getData() {
                     });
                     // Push the marker to our array of markers.
                     locationMarkers.push(marker);
-                    console.log("Marker: " + JSON.stringify(locationMarkers));
+                    // console.log("Marker: " + JSON.stringify(locationMarkers));
                     // marker.setMap(map);
-
+                    marker.addListener('click', function() {
+                        window.location.hash = "cards";
+                  });
 
                     // ======= END MARKER CREATION ===========
+
+
+
 
                     // ======= TEMPLATE CREATION ==============
 
 
                 } else {
-                    console.log("This bar is not close to you");
+                    // console.log("This bar is not close to you");
                 }
 
             });
 
         })
+        // the function showmarkers is fired after the entire for each loop is running and done pulling out appropriate categories
         .then(function() {
             showMarkers();
         });
@@ -293,137 +299,3 @@ function getData() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ======== 1st GOOGLE API AJAX FUNCTION===========
-//====== Draft - by Nigel ==========
-// googlePlacesPull();
-// var petStores = [];
-
-// function googlePlacesPull() {
-//       // Add the GOOGLE QUERY SEARCH url 
-//         // Starting with Pet Store search
-//         var searchParam = "pet_store"; 
-//          // var searchParam = "veterinary_care";
-//          // This current search will only yeild 19 results. Use the next_page_token to get more results
-//         var queryURL= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.2672,-97.7431&radius=50000&type=" + searchParam + "&key=AIzaSyBBH_dVCGulO-Q4XVW2VVGPhjv-Q5J8i5E"
-
-
-//         // Ajax call that pulls the data from the api
-//         $.ajax({
-//                 headers: {
-//                   'Access-Control-Allow-Origin': '*'
-//                 },
-//                 url: queryURL,
-//                 method: 'GET'
-//             })
-//             .done(function(response) {
-//               // Logs entire response
-//               // console.log("This is the GOOGLE place response: " + JSON.stringify(response));
-
-//               //Sets the variable results = the entire data set coming from the API
-//               var data = response.results; 
-
-//               console.log(data);
-
-//               // Create a for loop to loop through the results then pull out the various tags that we need
-//               for (var i=0; i < data.length; i++){
-//                 var name = data[i].name;
-//                 var placeId = data[i].place_id;
-//                 var location = data[i].geometry.location;
-
-//                 // Store this in an objec and pushes it to the empty array called petStores
-//                 petStores.push({name: name, placeId: placeId, location: location, id: i });
-//                 // push these objects to an array which will then be pushed to the firebase database
-
-//               }
-//               googleDetails(petStores);
-//       });
-//     }
-
-// // ===== 2ND function that pulls the data from the first petStores search and populates more detailed data =====
-
-// function googleDetails(stores) {
-//       // API DATA
-
-//       // Add the GOOGLE QUERY SEARCH url 
-//         // Starting with Pet Store search details 
-//         // ChIJM89u8izKRIYRQ1idTtNkf9o  test place id for a pet store
-//     for (var i=0; i < stores.length; i++){
-//         var petStoreId = petStores[i].placeId;
-//         var queryURL= "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + petStoreId + "&key=AIzaSyBBH_dVCGulO-Q4XVW2VVGPhjv-Q5J8i5E"
-
-
-//         // Ajax call that pulls the data from the api
-//         $.ajax({
-//                 headers: {
-//                   'Access-Control-Allow-Origin': '*'
-//                 },
-//                 // async: false,
-//                 url: queryURL,
-//                 method: 'GET'
-//             })
-//             // Create this as a function outside of the ajax call
-//             .done(placeDetails); 
-//     }
-//   } 
-
-// function placeDetails(response){
-//  // Logs entire response
-//               // console.log("This is the 2nd GOOGLE place response: " + JSON.stringify(response));
-
-//               //Sets the variable results = the entire data set coming from the API
-//               var dataSetTwo = response.result; 
-
-//               console.log(dataSetTwo);
-
-//               // Create a for loop to loop through the results then pull out the various tags that we need
-//               // for (var i=0; i < petStores.length; i++){
-//                 var address = dataSetTwo[i].formatted_address;
-//                 var phone = dataSetTwo[i].formatted_phone_number;
-//                 var icon = dataSetTwo[i].icon;
-//                 var website = dataSetTwo[i].website;
-
-
-//                 // Store this in an objec and pushes it to the empty array called petStores
-//                 petStores.push({address: address, phone: phone, icon: icon, website: website});
-//                 // push these objects to an array which will then be pushed to the firebase database
-
-
-// }
-
-
-
-// Empties the cards view  before adding a new buttons
-// $('#cardsAppearHere').empty();
-
-// NOT USING THE UNDERSCORE LIBRARY
-// Loops through array and prints out all items to the screen
-//          for (var i = 0; i < results.length; i++) {
-
-//           // creates the materialize 'card'
-// var starDiv = $('<div class="col s12 m4 l3"><div class="card"><div class="card-image"><img src="'+ results[i].images.fixed_height_small_still.url + '" data-still="'+ results[i].images.fixed_height_small_still.url+ '" data-animate="'+ results[i].images.fixed_height_small.url +'" data-state="still"></div><div class="card-content"><p class="rate"> Rating: '  + results[i].rating + '</p></div></div></div>');
-//              // Writes the card to the page
-//              $('#gifsAppearHere').prepend(starDiv);
-
-// }
-
-// using Underscore.js and template the image changes. 
-// var $gifs = $('#gifsAppearHere');
-// // Targeting the underscore template housed in the html
-// var $giphyTemplate = _.template($('#giphyTmpl').html());
-// // for each loop similar to that used in Star wars game
-// results.forEach(function(result) {
-//       $gifs.prepend($giphyTemplate({result: result }));
-//   });
